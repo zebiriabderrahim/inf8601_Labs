0c08bcaa36d51400c7529083f18ddc3a2f9caa5d
zebiriabderrahimzebiriabderrahim <abderrahim.zebiri@polymtl.ca>
2024-09-13 14:31:48 -0400

    Refactor code to improve pthread parallel pipeline implementation

--- Lab01/source/pipeline-pthread.c
+++ Lab01/source/pipeline-pthread.c
@@ -1,237 +1,182 @@
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <threads.h>
+#include <unistd.h>
 
 #include "filter.h"
 #include "pipeline.h"
 #include "queue.h"
 
 #define QUEUE_SIZE 100
-#define NUM_SCALE_THREADS 27
-#define NUM_PIXEL_THREADS 27
-#define NUM_SAVE_THREADS 27
-
-// Debug macro
-#if 0
-#define DEBUG_PRINT(fmt, ...) \
-    do { fprintf(stderr, "[%s:%d] " fmt, __func__, __LINE__, ##__VA_ARGS__); } while (0)
-
-#else
-#define DEBUG_PRINT(fmt, ...){}
-#endif
 
 struct thread_args {
-    queue_t* input_queue;
-    queue_t** output_queues;
-    int num_output_queues;
-    int thread_id;
+  queue_t *input_queue;
+  queue_t **output_queues;
+  int num_output_queues;
+  int thread_id;
 };
 
 struct load_args {
-    image_dir_t* image_dir;
-    queue_t** output_queues;
-    int num_output_queues;
-    int thread_id;
+  image_dir_t *image_dir;
+  queue_t **output_queues;
+  int num_output_queues;
+  int thread_id;
 };
 
 struct save_args {
-    queue_t* input_queue;
-    image_dir_t* image_dir;
-    int thread_id;
+  queue_t *input_queue;
+  image_dir_t *image_dir;
+  int thread_id;
 };
 
-void* image_load_wrapper(void* arg) {
-    struct load_args* args = (struct load_args*)arg;
-    int image_count = 0;
-    int next_queue = 0;
-    while (1) {
-        image_t* image = image_dir_load_next(args->image_dir);
-        if (image == NULL) {
-            for (int i = 0; i < args->num_output_queues; i++) {
-                queue_push(args->output_queues[i], NULL);
-            }
-            DEBUG_PRINT("Load thread: Pushed NULL to all queues, terminating\n");
-            break;
-        }
-        queue_push(args->output_queues[next_queue], image);
-        next_queue = (next_queue + 1) % args->num_output_queues;
-        image_count++;
-        DEBUG_PRINT("Load thread: Loaded and pushed image %d\n", image_count);
-    }
-    return NULL;
+int calculate_optimal_threads_num(long num_cores) {
+  int optimal_threads = (num_cores * 12) / 4;
+  return (optimal_threads < 2)    ? 2
+         : (optimal_threads > 27) ? 27
+                                  : optimal_threads;
 }
 
-void* filter_scale_up_wrapper(void* arg) {
-    struct thread_args* args = (struct thread_args*)arg;
-    int processed_count = 0;
-    int next_queue = 0;
-    while (1) {
-        image_t* image = queue_pop(args->input_queue);
-        if (image == NULL) {
-            for (int i = 0; i < args->num_output_queues; i++) {
-                queue_push(args->output_queues[i], NULL);
-            }
-            DEBUG_PRINT("Scale thread %d: Received NULL, pushed NULL to all queues, terminating\n", args->thread_id);
-            break;
-        }
-        image_t* scaled_image = filter_scale_up(image, 3);
-        image_destroy(image);
-        queue_push(args->output_queues[next_queue], scaled_image);
-        next_queue = (next_queue + 1) % args->num_output_queues;
-        processed_count++;
-        DEBUG_PRINT("Scale thread %d: Processed image %d\n", args->thread_id, processed_count);
+void *image_load_wrapper(void *arg) {
+  struct load_args *args = (struct load_args *)arg;
+  int image_count = 0;
+  while (1) {
+    image_t *image = image_dir_load_next(args->image_dir);
+    if (image == NULL) {
+      for (int i = 0; i < args->num_output_queues; i++) {
+        queue_push(args->output_queues[i], NULL);
+      }
+      break;
     }
-    return NULL;
+    queue_push(args->output_queues[image_count % args->num_output_queues],
+               image);
+    image_count++;
+  }
+  return NULL;
 }
 
-void* filter_add_pixel_wrapper(void* arg) {
-    pixel_t pixel = {.bytes = {0, 0, 0, 0}};
-    struct thread_args* args = (struct thread_args*)arg;
-    int processed_count = 0;
-    int next_queue = 0;
-    while (1) {
-        image_t* image = queue_pop(args->input_queue);
-        if (image == NULL) {
-            for (int i = 0; i < args->num_output_queues; i++) {
-                queue_push(args->output_queues[i], NULL);
-            }
-            DEBUG_PRINT("Pixel thread %d: Received NULL, pushed NULL to all queues, terminating\n", args->thread_id);
-            break;
-        }
-        pixel.bytes[0] = (unsigned char)((4 * (image->id + 1)) % 256);
-        image_t* pixel_added_image = filter_add_pixel(image, &pixel);
-        image_destroy(image);
-        queue_push(args->output_queues[next_queue], pixel_added_image);
-        next_queue = (next_queue + 1) % args->num_output_queues;
-        processed_count++;
-        DEBUG_PRINT("Pixel thread %d: Processed image %d\n", args->thread_id, processed_count);
+void *filter_scale_up_wrapper(void *arg) {
+  struct thread_args *args = (struct thread_args *)arg;
+  while (1) {
+    image_t *image = queue_pop(args->input_queue);
+    if (image == NULL) {
+      queue_push(args->output_queues[args->thread_id], NULL);
+      break;
     }
-    return NULL;
+    image_t *scaled_image = filter_scale_up(image, 3);
+    image_destroy(image);
+    queue_push(args->output_queues[args->thread_id], scaled_image);
+  }
+  return NULL;
 }
 
-void* image_save_wrapper(void* arg) {
-    struct save_args* args = (struct save_args*)arg;
-    int saved_count = 0;
-    while (1) {
-        image_t* image = queue_pop(args->input_queue);
-        if (image == NULL) {
-            DEBUG_PRINT("Save thread %d: Received NULL, terminating\n", args->thread_id);
-            break;
-        }
-        image_dir_save(args->image_dir, image);
-        printf(".");
-        fflush(stdout);
-        image_destroy(image);
-        saved_count++;
-        DEBUG_PRINT("Save thread %d: Saved image %d\n", args->thread_id, saved_count);
+void *filter_add_pixel_wrapper(void *arg) {
+  pixel_t pixel = {.bytes = {0, 0, 0, 0}};
+  struct thread_args *args = (struct thread_args *)arg;
+  while (1) {
+    image_t *image = queue_pop(args->input_queue);
+    if (image == NULL) {
+      queue_push(args->output_queues[args->thread_id], NULL);
+      break;
     }
-    return NULL;
+    pixel.bytes[0] = (unsigned char)((4 * (image->id + 1)) % 256);
+    image_t *pixel_added_image = filter_add_pixel(image, &pixel);
+    image_destroy(image);
+    queue_push(args->output_queues[args->thread_id], pixel_added_image);
+  }
+  return NULL;
 }
 
-int pipeline_pthread(image_dir_t* image_dir) {
-    DEBUG_PRINT("Starting pipeline_pthread\n");
-    queue_t* loaded_img_queue[NUM_SCALE_THREADS];
-    queue_t* scaled_img_queue[NUM_PIXEL_THREADS];
-    queue_t* pixel_added_img_queue[NUM_SAVE_THREADS];
-    
-    for (int i = 0; i < NUM_SCALE_THREADS; i++) {
-        loaded_img_queue[i] = queue_create(QUEUE_SIZE);
-        if (!loaded_img_queue[i]) {
-            fprintf(stderr, "Failed to create loaded_img_queue %d\n", i);
-            goto cleanup;
-        }
+void *image_save_wrapper(void *arg) {
+  struct save_args *args = (struct save_args *)arg;
+  while (1) {
+    image_t *image = queue_pop(args->input_queue);
+    if (image == NULL) {
+      break;
     }
-    for (int i = 0; i < NUM_PIXEL_THREADS; i++) {
-        scaled_img_queue[i] = queue_create(QUEUE_SIZE);
-        if (!scaled_img_queue[i]) {
-            fprintf(stderr, "Failed to create scaled_img_queue %d\n", i);
-            goto cleanup;
-        }
-    }
-    for (int i = 0; i < NUM_SAVE_THREADS; i++) {
-        pixel_added_img_queue[i] = queue_create(QUEUE_SIZE);
-        if (!pixel_added_img_queue[i]) {
-            fprintf(stderr, "Failed to create pixel_added_img_queue %d\n", i);
-            goto cleanup;
-        }
-    }
+    image_dir_save(args->image_dir, image);
+    printf(".");
+    fflush(stdout);
+    image_destroy(image);
+  }
+  return NULL;
+}
 
-    pthread_t load_thread;
-    pthread_t scale_threads[NUM_SCALE_THREADS];
-    pthread_t pixel_threads[NUM_PIXEL_THREADS];
-    pthread_t save_threads[NUM_SAVE_THREADS];
+int pipeline_pthread(image_dir_t *image_dir) {
+  long num_cores = sysconf(_SC_NPROCESSORS_ONLN);
+  printf("Number of cores: %ld\n", num_cores);
+  const int NUM_THREADS = calculate_optimal_threads_num(num_cores);
+  printf("Optimal number of threads: %d\n", NUM_THREADS);
 
-    struct load_args load_args = {image_dir, loaded_img_queue, NUM_SCALE_THREADS, 0};
-    struct thread_args scale_args[NUM_SCALE_THREADS];
-    struct thread_args pixel_args[NUM_PIXEL_THREADS];
-    struct save_args save_args[NUM_SAVE_THREADS];
+  queue_t *loaded_img_queue[NUM_THREADS];
+  queue_t *scaled_img_queue[NUM_THREADS];
+  queue_t *pixel_added_img_queue[NUM_THREADS];
 
-    // Create load thread
-    if (pthread_create(&load_thread, NULL, image_load_wrapper, &load_args) != 0) {
-        fprintf(stderr, "Error creating load thread\n");
-        goto cleanup;
-    }
-    DEBUG_PRINT("Created load thread\n");
+  pthread_t load_thread;
+  pthread_t scale_threads[NUM_THREADS];
+  pthread_t pixel_threads[NUM_THREADS];
+  pthread_t save_threads[NUM_THREADS];
 
-    // Create scale threads
-    for (int i = 0; i < NUM_SCALE_THREADS; i++) {
-        scale_args[i] = (struct thread_args){loaded_img_queue[i], scaled_img_queue, NUM_PIXEL_THREADS, i};
-        if (pthread_create(&scale_threads[i], NULL, filter_scale_up_wrapper, &scale_args[i]) != 0) {
-            fprintf(stderr, "Error creating scale thread %d\n", i);
-            goto cleanup;
-        }
-        DEBUG_PRINT("Created scale thread %d\n", i);
-    }
+  struct load_args load_args = {image_dir, loaded_img_queue, NUM_THREADS, 0};
+  struct thread_args scale_args[NUM_THREADS];
+  struct thread_args pixel_args[NUM_THREADS];
+  struct save_args save_args[NUM_THREADS];
 
-    // Create pixel addition threads
-    for (int i = 0; i < NUM_PIXEL_THREADS; i++) {
-        pixel_args[i] = (struct thread_args){scaled_img_queue[i], pixel_added_img_queue, NUM_SAVE_THREADS, i};
-        if (pthread_create(&pixel_threads[i], NULL, filter_add_pixel_wrapper, &pixel_args[i]) != 0) {
-            fprintf(stderr, "Error creating pixel thread %d\n", i);
-            goto cleanup;
-        }
-        DEBUG_PRINT("Created pixel thread %d\n", i);
+  // Create queues
+  for (int i = 0; i < NUM_THREADS; i++) {
+    loaded_img_queue[i] = queue_create(QUEUE_SIZE);
+    scaled_img_queue[i] = queue_create(QUEUE_SIZE);
+    pixel_added_img_queue[i] = queue_create(QUEUE_SIZE);
+    if (!loaded_img_queue[i] || !scaled_img_queue[i] ||
+        !pixel_added_img_queue[i]) {
+      fprintf(stderr, "Failed to create queue %d\n", i);
+      goto cleanup;
     }
+  }
 
-    // Create save threads
-    for (int i = 0; i < NUM_SAVE_THREADS; i++) {
-        save_args[i] = (struct save_args){pixel_added_img_queue[i], image_dir, i};
-        if (pthread_create(&save_threads[i], NULL, image_save_wrapper, &save_args[i]) != 0) {
-            fprintf(stderr, "Error creating save thread %d\n", i);
-            goto cleanup;
-        }
-        DEBUG_PRINT("Created save thread %d\n", i);
-    }
+  // Create load thread
+  if (pthread_create(&load_thread, NULL, image_load_wrapper, &load_args) != 0) {
+    fprintf(stderr, "Error creating load thread\n");
+    goto cleanup;
+  }
 
-    // Join threads
-    DEBUG_PRINT("Waiting for threads to complete\n");
-    pthread_join(load_thread, NULL);
-    DEBUG_PRINT("Load thread joined\n");
-    for (int i = 0; i < NUM_SCALE_THREADS; i++) {
-        pthread_join(scale_threads[i], NULL);
-        DEBUG_PRINT("Scale thread %d joined\n", i);
-    }
-    for (int i = 0; i < NUM_PIXEL_THREADS; i++) {
-        pthread_join(pixel_threads[i], NULL);
-        DEBUG_PRINT("Pixel thread %d joined\n", i);
-    }
-    for (int i = 0; i < NUM_SAVE_THREADS; i++) {
-        pthread_join(save_threads[i], NULL);
-        DEBUG_PRINT("Save thread %d joined\n", i);
-    }
+  // Create scale, pixel, and save threads
+  for (int i = 0; i < NUM_THREADS; i++) {
+    scale_args[i] = (struct thread_args){loaded_img_queue[i], scaled_img_queue,
+                                         NUM_THREADS, i};
+    pixel_args[i] = (struct thread_args){scaled_img_queue[i],
+                                         pixel_added_img_queue, NUM_THREADS, i};
+    save_args[i] = (struct save_args){pixel_added_img_queue[i], image_dir, i};
 
-    printf("\n");
+    if (pthread_create(&scale_threads[i], NULL, filter_scale_up_wrapper,
+                       &scale_args[i]) != 0 ||
+        pthread_create(&pixel_threads[i], NULL, filter_add_pixel_wrapper,
+                       &pixel_args[i]) != 0 ||
+        pthread_create(&save_threads[i], NULL, image_save_wrapper,
+                       &save_args[i]) != 0) {
+      fprintf(stderr, "Error creating thread %d\n", i);
+      goto cleanup;
+    }
+  }
+
+  // Join threads
+  pthread_join(load_thread, NULL);
+  for (int i = 0; i < NUM_THREADS; i++) {
+    pthread_join(scale_threads[i], NULL);
+    pthread_join(pixel_threads[i], NULL);
+    pthread_join(save_threads[i], NULL);
+  }
+
+  printf("\n");
 
 cleanup:
-    for (int i = 0; i < NUM_SCALE_THREADS; i++) {
-        if (loaded_img_queue[i]) queue_destroy(loaded_img_queue[i]);
-    }
-    for (int i = 0; i < NUM_PIXEL_THREADS; i++) {
-        if (scaled_img_queue[i]) queue_destroy(scaled_img_queue[i]);
-    }
-    for (int i = 0; i < NUM_SAVE_THREADS; i++) {
-        if (pixel_added_img_queue[i]) queue_destroy(pixel_added_img_queue[i]);
-    }
-    DEBUG_PRINT("Pipeline completed\n");
-    return 0;
+  for (int i = 0; i < NUM_THREADS; i++) {
+    if (loaded_img_queue[i])
+      queue_destroy(loaded_img_queue[i]);
+    if (scaled_img_queue[i])
+      queue_destroy(scaled_img_queue[i]);
+    if (pixel_added_img_queue[i])
+      queue_destroy(pixel_added_img_queue[i]);
+  }
+  return 0;
 }
